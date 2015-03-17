d0ec6456cf68f10ceb9102564fbb2a3558a39790
elenadmitrieva <elena.an.dmitrieva@gmail.com>
2015-03-17 16:29:47 +0200

    Fixed of convert price. Refactoring.

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ActiveRecord::Base
   has_many :orders
   validates :name, :description, :image_url, :price, presence: true
 
-  def self.convert_price(pr, exchange_rate)
-    pr.price = pr.price * exchange_rate
+  def convert_price(exchange_rate)
+    self.price = self.price * exchange_rate
   end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,30 +1,30 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe Product, :type => :model do
 
-  let!(:product1_test) { Product.new(name: 'blouse', description: 'Bright top', price: '10', image_url: 'blouse.jpg') }
+  let(:product1_test) { Product.new(name: 'blouse', description: 'Bright top', price: '10', image_url: 'blouse.jpg') }
 
-  it "is not valid without a name" do
+  it 'is not valid without a name' do
     product1_test.name = nil
     expect(product1_test).not_to be_valid
   end
 
-  it "is not valid without a description" do
+  it 'is not valid without a description' do
     product1_test.description = nil
     expect(product1_test).not_to be_valid
   end
 
-  it "is not valid without a price" do
+  it 'is not valid without a price' do
     product1_test.price = nil
     expect(product1_test).not_to be_valid
   end
 
-  it "is not valid without a image_url" do
+  it 'is not valid without a image_url' do
     product1_test.image_url = nil
     expect(product1_test).not_to be_valid
   end
 
   it 'convert price' do
-    expect(Product.convert_price(product1_test, 20)).to eq (200)
+    expect(product1_test.convert_price(20)).to eq (200)
   end
 end
